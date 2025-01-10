c64c03c6956df70edb7aa540906fe54433f05cee
CipherKode <kodecipher@gmail.com>
2025-01-10 21:18:56 +0530

    Snehal | Starting the api design

---
+++ src/interfaces/req/StartChatFormRequest.ts
@@ -0,0 +1,5 @@
+export interface StartChatFormRequest {
+  name: string;
+  email: string;
+  phone: string;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,41 @@
+import express from 'express';
+import cookieParser from 'cookie-parser'
+import {
+  useContainer,
+  useExpressServer
+} from 'routing-controllers'
+import { Container } from 'typedi'
+import 'reflect-metadata'
 
+class Server {
+  private static app = express();
+
+  private static addMiddleWares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+  }
+
+  private static getControllers() {
+    return [];
+  }
+
+  private static configureWithRoutingController() {
+    useContainer(Container);
+    useExpressServer(this.app, {
+      controllers: this.getControllers()
+    });
+  }
+
+  static start(port : number) {
+    this.addMiddleWares();
+    this.configureWithRoutingController();
+
+    this.app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    })
+  }
+}
+
+// starting the server
+Server.start(4040);
